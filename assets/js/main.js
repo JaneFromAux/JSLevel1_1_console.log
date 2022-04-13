b3da83fa1f87ccdcef6a5d07ff7506adb939bc76
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-13 15:56:32 +0200

    Aufgabe erledigt

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+// console.log(document.URL);
+console.log("Hello, World!")
+console.log("Anton");
+let name = "Anton";
+console.log(name);
+let age = 28;
+console.log(age);
+let job = 'trainer';
+console.log(job);
+let married = true;
+console.log(married);
+
+console.log(name + ' ist ' + age + ' Jahre alt, von Beruf ' + job + ' und ist verheiratet: ' + married);
